15a29a67924ef60eb6b210ba427fe7580858334b
Colin McKechney <cmckechn@nd.edu>
2021-06-22 16:34:14 -0400

    changed file write location

--- bin/timing.py
+++ bin/timing.py
@@ -2,9 +2,9 @@
 import subprocess
 import csv
 
-firstMultFile = open('firstMult.csv', 'w')
-secondMultFile = open('secondMult.csv','w')
-sgxFile = open('sgx.csv','w')
+firstMultFile = open('result/firstMult.csv', 'w')
+secondMultFile = open('result/secondMult.csv','w')
+sgxFile = open('result/sgx.csv','w')
 
 firstMultWriter = csv.writer(firstMultFile,csv.get_dialect('excel'))
 secondMultWriter = csv.writer(secondMultFile,csv.get_dialect('excel'))
@@ -21,20 +21,17 @@ for i in range(2,13):
         result = subprocess.run(['./firstMult','-p',f'{i}','-n',f'{dN}'],stdout = subprocess.PIPE)
         firstMultRow = row + bytes.decode(result.stdout).strip().split('\n')
         firstMultWriter.writerow(firstMultRow)
-        print(f'{i} {j} first mult complete, results:')
-        print(result.stdout)
+        print(f'{i} {j} first mult complete')
 
         result = subprocess.run(['./secondMult','-p',f'{i}','-n',f'{dN}'],stdout=subprocess.PIPE)
         secondMultRow = row + bytes.decode(result.stdout).strip().split('\n')
         secondMultWriter.writerow(secondMultRow)
-        print(f'{i} {j} second mult complete, results:')
-        print(result.stdout)
+        print(f'{i} {j} second mult complete')
 
         result = subprocess.run(['./pal_loader','./inverse','-p',f'{i}','-n',f'{dN}'],stdout=subprocess.PIPE)
         sgxRow = row + bytes.decode(result.stdout).strip().split('\n')
         sgxWriter.writerow(sgxRow)
-        print(f'{i} {j} inverse complete, results: ')
-        print(result.stdout)
+        print(f'{i} {j} inverse complete')
 
 for i in range(1000000,10000000,1000000):
     row = [dP,i]
@@ -44,20 +41,17 @@ for i in range(1000000,10000000,1000000):
         result = subprocess.run(['./firstMult','-p',f'{dP}','-n',f'{i}'],stdout = subprocess.PIPE)
         firstMultRow = row + bytes.decode(result.stdout).strip().split('\n')
         firstMultWriter.writerow(firstMultRow)
-        print(f'{i} {j} first mult complete, results:')
-        print(result.stdout)
+        print(f'{i} {j} first mult complete')
 
         result = subprocess.run(['./secondMult','-p',f'{dP}','-n',f'{i}'],stdout=subprocess.PIPE)
         secondMultRow = row + bytes.decode(result.stdout).strip().split('\n')
         secondMultWriter.writerow(secondMultRow)
-        print(f'{i} {j} second mult complete, results:')
-        print(result.stdout)
+        print(f'{i} {j} second mult complete')
 
         result = subprocess.run(['./pal_loader','./inverse','-p',f'{dP}','-n',f'{i}'],stdout=subprocess.PIPE)
         sgxRow = row + bytes.decode(result.stdout).strip().split('\n')
         sgxWriter.writerow(sgxRow)
-        print(f'{i} {j} inverse complete, results: ')
-        print(result.stdout)
+        print(f'{i} {j} inverse complete')
 
 
         
